60bad0fb26471779c390b20321676d22c9733bc9
Refayet <13129292+Refayatul@users.noreply.github.com>
2025-07-31 11:44:05 +0600

    initial v1 flyway setup

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+	// No additional code needed here to run the application
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
